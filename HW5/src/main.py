bed276149b50c94eea1d8636de3261a53e4df3db
Emmanuel Norambuena <eanorambuena@uc.cl>
2025-11-13 11:42:15 +0000

    Implement Bitcoin Script Operations and Transaction Handling
    - Added op.py to define Bitcoin script operations including op_verify, op_dup, op_equal, op_add, op_hash160, op_hash256, op_checksig, and their verification counterparts.
    - Introduced scriptSimplified.py for handling script parsing, serialization, and evaluation, including P2PKH and P2SH script creation functions.
    - Developed txP2PKH.py to manage Bitcoin transactions, including transaction creation, signing, and verification processes.
    - Implemented TxFetcher class for fetching transactions from a full node and caching them for reuse.
    - Enhanced transaction handling with classes Tx, TxIn, and TxOut for managing transaction inputs and outputs.
    - Added comprehensive testing and debugging outputs for transaction creation and script evaluation.

---
+++ HW5/src/main.py
@@ -0,0 +1,12 @@
+from block import FullBlock
+
+
+# Implement the procedure to get the first 20 blocks of bitcoin's testnet here.
+# The blocks should be returned as an ordered list starting from the original.
+def main() -> list[FullBlock]:
+    return []
+
+
+if __name__ == '__main__':
+    for b in main():
+        print(b)
